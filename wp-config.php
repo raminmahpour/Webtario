9efe97e10994bb3ec0bac30750fdad475fa55ffe
Ramin <ramin@Ramins-MacBook-Pro.local>
2019-05-12 21:54:41 -0400

    okshod

--- wp-config.php
+++ wp-config.php
@@ -15,7 +15,7 @@
  *
  * @link https://codex.wordpress.org/Editing_wp-config.php
  *
- * @package WordPress
+ * @package WordPress123
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
